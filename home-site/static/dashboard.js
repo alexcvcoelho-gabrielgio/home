2bdb59d6eadaa6ae522c4ceb32ab593a40ae8b8e
gabriel giovanini <gabrielgio@localhost.localdomain>
2017-11-28 23:12:23 -0200

    Adds animation

--- home-site/static/dashboard.js
+++ home-site/static/dashboard.js
@@ -1,5 +1,11 @@
-markers = []
+SlidingMarker.initializeGlobally();
+
+markers = [];
+dicMarker = {};
 var facens = {lat: -23.469742, lng: -47.429808};
+
+var directionsService = new google.maps.DirectionsService();
+var directionsDisplay = new google.maps.DirectionsRenderer();
 
 map = new google.maps.Map(document.getElementById('map'), {
     zoom: 5,
@@ -14,18 +20,36 @@ function addMarker(location) {
     markers.push(marker);
 }
 
-dicMarker = {}
-
 updateMarker = function (data) {
     if (dicMarker[data.uuid]) {
         dicMarker[data.uuid].setPosition({lat: data.lat, lng: data.long})
+
     }
     else {
         dicMarker[data.uuid] = new google.maps.Marker({
             position: {lat: data.lat, lng: data.long},
             map: map
         });
+        dicMarker[data.uuid].tag = data;
+        dicMarker[data.uuid].addListener('click', function () {
+            drawLine(dicMarker[data.uuid].getPosition(), {lat: data["des-lat"], lng: data["des-long"]})
+        });
     }
+}
+
+drawLine = function (or, des) {
+    directionsDisplay.setMap(map);
+    var request = {
+        travelMode: google.maps.TravelMode.DRIVING,
+        origin: or,
+        destination: des
+    };
+
+    directionsService.route(request, function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(result);
+        }
+    });
 }
 
 clearMap = function () {
